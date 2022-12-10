f3b58fcd5306fbe96fce1fd470e0898912d744c5
gabiiing13 <gabiiing13@gmail.com>
2022-12-11 00:19:47 +0700

    add authentication

--- recycle/lib/drawer.dart
+++ recycle/lib/drawer.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:recycle/Alfredo/pages/home_page.dart';
-import 'package:recycle/Authentication/page/login_page.dart';
+import 'package:recycle/Authentication/page/authentication.dart';
 import 'package:recycle/main.dart';
 import 'package:recycle/drawer.dart';
 import 'package:recycle/event/page/event_your.dart';
-
 class ExternalDrawer extends StatefulWidget {
   const ExternalDrawer({super.key});
-
   @override
   State<StatefulWidget> createState() => _ExternalDrawer();
 }
-
 class _ExternalDrawer extends State<ExternalDrawer> {
   @override
   Widget build(BuildContext context) {
@@ -20,10 +16,21 @@ class _ExternalDrawer extends State<ExternalDrawer> {
       child: Column(
         children: [
           ListTile(
-            title: const Text("Home Page"),
+            title: const Text("Main"),
             onTap: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => const HomePage()));
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const MyHomePage(title: "Test")));
+            },
+          ),
+          ListTile(
+            title: const Text("Authentication"),
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AuthenticationPage()));
             },
           ),
           ListTile(
