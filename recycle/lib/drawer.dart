932bd2fac060d9d99d607c289c208a6b2cb07f81
gabiiing13 <gabiiing13@gmail.com>
2022-12-11 00:40:45 +0700

    change drawer and main

--- recycle/lib/drawer.dart
+++ recycle/lib/drawer.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:recycle/Authentication/page/authentication.dart';
+import 'package:recycle/Alfredo/pages/home_page.dart';
+import 'package:recycle/Authentication/page/login_page.dart';
 import 'package:recycle/main.dart';
 import 'package:recycle/drawer.dart';
-import 'package:recycle/event/page/event_your.dart';
+
 class ExternalDrawer extends StatefulWidget {
   const ExternalDrawer({super.key});
+
   @override
   State<StatefulWidget> createState() => _ExternalDrawer();
 }
+
 class _ExternalDrawer extends State<ExternalDrawer> {
   @override
   Widget build(BuildContext context) {
@@ -16,30 +19,10 @@ class _ExternalDrawer extends State<ExternalDrawer> {
       child: Column(
         children: [
           ListTile(
-            title: const Text("Main"),
+            title: const Text("Home Page"),
             onTap: () {
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const MyHomePage(title: "Test")));
-            },
-          ),
-          ListTile(
-            title: const Text("Authentication"),
-            onTap: () {
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const AuthenticationPage()));
-            },
-          ),
-          ListTile(
-            title: const Text("Your Events"),
-            onTap: () {
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const YourEventPage()));
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const HomePage()));
             },
           ),
         ],
