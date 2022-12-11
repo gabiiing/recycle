360282b970e40b8ff4c42cb87a014c0fb1110a38
gabiiing13 <gabiiing13@gmail.com>
2022-12-11 21:28:50 +0700

    fetch get post event done

--- recycle/lib/event/model/event.dart
+++ recycle/lib/event/model/event.dart
@@ -1,119 +1,118 @@
 // To parse this JSON data, do
-//
+
 //     final event = eventFromJson(jsonString);
 
-// import 'package:meta/meta.dart';
-// import 'dart:convert';
+import 'package:meta/meta.dart';
+import 'dart:convert';
 
-// List<Event> eventFromJson(String str) => List<Event>.from(json.decode(str).map((x) => Event.fromJson(x)));
+List<Event> eventFromJson(String str) => List<Event>.from(json.decode(str).map((x) => Event.fromJson(x)));
 
-// String eventToJson(List<Event> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String eventToJson(List<Event> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-// class Event {
-//     Event({
-//         required this.model,
-//         required this.pk,
-//         required this.fields,
-//     });
+class Event {
+    Event({
+        required this.model,
+        required this.pk,
+        required this.fields,
+    });
 
-//     String model;
-//     int pk;
-//     Fields fields;
+    String model;
+    int pk;
+    Fields fields;
 
-//     factory Event.fromJson(Map<String, dynamic> json) => Event(
-//         model: json["model"],
-//         pk: json["pk"],
-//         fields: Fields.fromJson(json["fields"]),
-//     );
+    factory Event.fromJson(Map<String, dynamic> json) => Event(
+        model: json["model"],
+        pk: json["pk"],
+        fields: Fields.fromJson(json["fields"]),
+    );
 
-//     Map<String, dynamic> toJson() => {
-//         "model": model,
-//         "pk": pk,
-//         "fields": fields.toJson(),
-//     };
-// }
-
-// class Fields {
-//     Fields({
-//         required this.user,
-//         required this.createDate,
-//         required this.startDate,
-//         required this.finishDate,
-//         required this.title,
-//         required this.brief,
-//         required this.description,
-//     });
-
-//     int user;
-//     DateTime createDate;
-//     DateTime startDate;
-//     DateTime finishDate;
-//     String title;
-//     String brief;
-//     String description;
-
-//     factory Fields.fromJson(Map<String, dynamic> json) => Fields(
-//         user: json["user"],
-//         createDate: DateTime.parse(json["create_date"]),
-//         startDate: DateTime.parse(json["start_date"]),
-//         finishDate: DateTime.parse(json["finish_date"]),
-//         title: json["title"],
-//         brief: json["brief"],
-//         description: json["description"],
-//     );
-
-//     Map<String, dynamic> toJson() => {
-//         "user": user,
-//         "create_date": createDate.toIso8601String(),
-//         "start_date": startDate.toIso8601String(),
-//         "finish_date": finishDate.toIso8601String(),
-//         "title": title,
-//         "brief": brief,
-//         "description": description,
-//     };
-// }
-
-
-class Event{
-  late int id;
-  late String title;
-  late String description;
-  late String brief;
-  late DateTime startDate;
-  late DateTime finishDate;
-  late DateTime createDate;
-  late int user;
-
-  Event({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.brief,
-    required this.startDate,
-    required this.finishDate,
-    required this.createDate,
-    required this.user,
-  });
-
-  factory Event.fromJson(Map<String, dynamic> json) => Event(
-    id: json["id"],
-    title: json["title"],
-    description: json["description"],
-    brief: json["brief"],
-    startDate: DateTime.parse(json["start_date"]),
-    finishDate: DateTime.parse(json["finish_date"]),
-    createDate: DateTime.parse(json["create_date"]),
-    user: json["user"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "id": id,
-    "title": title,
-    "description": description,
-    "brief": brief,
-    "start_date": startDate,
-    "finish_date": finishDate,
-    "create_date": createDate,
-    "user": user,
-  };
+    Map<String, dynamic> toJson() => {
+        "model": model,
+        "pk": pk,
+        "fields": fields.toJson(),
+    };
 }
+
+class Fields {
+    Fields({
+        required this.user,
+        required this.createDate,
+        required this.startDate,
+        required this.finishDate,
+        required this.title,
+        required this.brief,
+        required this.description,
+    });
+
+    int user;
+    DateTime createDate;
+    DateTime startDate;
+    DateTime finishDate;
+    String title;
+    String brief;
+    String description;
+
+    factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+        user: json["user"],
+        createDate: DateTime.parse(json["create_date"]),
+        startDate: DateTime.parse(json["start_date"]),
+        finishDate: DateTime.parse(json["finish_date"]),
+        title: json["title"],
+        brief: json["brief"],
+        description: json["description"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "user": user,
+        "create_date": createDate.toIso8601String(),
+        "start_date": startDate.toIso8601String(),
+        "finish_date": finishDate.toIso8601String(),
+        "title": title,
+        "brief": brief,
+        "description": description,
+    };
+}
+
+
+// class Event{
+//   late int user;
+//   late String title;
+//   late String description;
+//   late String brief;
+//   late DateTime startDate;
+//   late DateTime finishDate;
+//   late DateTime createDate;
+
+//   Event({
+//     required this.id,
+//     required this.title,
+//     required this.description,
+//     required this.brief,
+//     required this.startDate,
+//     required this.finishDate,
+//     required this.createDate,
+//     required this.user,
+//   });
+
+//   factory Event.fromJson(Map<String, dynamic> json) => Event(
+//     id: json["id"],
+//     title: json["title"],
+//     description: json["description"],
+//     brief: json["brief"],
+//     startDate: DateTime.parse(json["start_date"]),
+//     finishDate: DateTime.parse(json["finish_date"]),
+//     createDate: DateTime.parse(json["create_date"]),
+//     user: json["user"],
+//   );
+
+//   Map<String, dynamic> toJson() => {
+//     "id": id,
+//     "title": title,
+//     "description": description,
+//     "brief": brief,
+//     "start_date": startDate,
+//     "finish_date": finishDate,
+//     "create_date": createDate,
+//     "user": user,
+//   };
+// }
---
+++ recycle/lib/event/page/event_form.dart
@@ -0,0 +1,300 @@
+import 'package:flutter/material.dart';
+import '../model/event.dart';
+import '../method/event_method.dart';
+import '../page/event_your.dart';
+import 'package:recycle/Authentication/page/login_page.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:recycle/drawer.dart';
+
+class EventForm extends StatefulWidget {
+  const EventForm({super.key});
+
+  @override
+  State<EventForm> createState() => _EventFormState();
+}
+
+class _EventFormState extends State<EventForm> {
+  final _EventFormKey = GlobalKey<FormState>();
+
+  String user = userData!['idUser'].toString();
+  String title = "";
+  String brief = "";
+  String description = "";
+  DateTime created_date = DateTime.now();
+  DateTime started_date = DateTime.now();
+  DateTime finished_date = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Create Event"),
+      ),
+      drawer: const ExternalDrawer(),
+      body: Form(
+        key: _EventFormKey,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      labelText: "Title",
+                      hintText: "Enter Title",
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter title";
+                      }
+                      title = value;
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      labelText: "Brief",
+                      hintText: "Enter Brief",
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter brief";
+                      }
+                      brief = value;
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      labelText: "Description",
+                      hintText: "Enter Description",
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter Description";
+                      }
+                      description = value;
+                      return null;
+                    },
+                  ),
+                ),
+                // make datetime picker
+                Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextButton(
+                        onPressed: () {
+                          DatePicker.showDateTimePicker(context,
+                              showTitleActions: true,
+                              minTime: DateTime(2021, 12, 5),
+                              maxTime: DateTime(2100, 12, 12),
+                              onChanged: (date) {
+                            print('change $date');
+                          }, onConfirm: (date) {
+                            print('confirm $date');
+                            started_date = date;
+                            print(started_date);
+                          },
+                              currentTime: DateTime.now(),
+                              locale: LocaleType.id);
+                        },
+                        child: Text(
+                          'set started date',
+                          style: TextStyle(color: Colors.blue),
+                        ))),
+                Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextButton(
+                        onPressed: () {
+                          DatePicker.showDateTimePicker(context,
+                              showTitleActions: true,
+                              minTime: DateTime(2021, 12, 5),
+                              maxTime: DateTime(2100, 12, 12),
+                              onChanged: (date) {
+                            print('change $date');
+                          }, onConfirm: (date) {
+                            print('confirm $date');
+                            finished_date = date;
+                            print(finished_date);
+                          },
+                              currentTime: DateTime.now(),
+                              locale: LocaleType.id);
+                        },
+                        child: Text(
+                          'set finished date',
+                          style: TextStyle(color: Colors.blue),
+                        ))),
+                ElevatedButton(
+                    child: Text(
+                      "Create Event",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async {
+                      _EventFormKey.currentState!.validate();
+                      if (_EventFormKey.currentState!.validate() ?? false) {
+                        print(started_date.runtimeType);
+                        print(finished_date.runtimeType);
+                        String date = started_date.toString().split(" ")[0];
+                        String time = started_date
+                            .toString()
+                            .split(" ")[1]
+                            .substring(0, 8);
+                        print(date);
+                        print(time);
+                        String sdate =date+ "T"+time;
+                        date = finished_date.toString().split(" ")[0];
+                        time = finished_date
+                            .toString()
+                            .split(" ")[1]
+                            .substring(0, 8);
+                        String fdate =date+ "T"+time;
+
+                        print(started_date.toString());
+
+                        await request.post(
+                            "https://pbp-d01.up.railway.app/event/add-event/", {
+                            // "http://127.0.0.1:8000/event/add-event/",
+                            // {
+                              "title": title,
+                              "brief": brief,
+                              "description": description,
+                              "start_date": sdate,
+                              "finish_date": fdate,
+                            }).then(
+                          (value) {
+                            return showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return Dialog(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    elevation: 15,
+                                    child: ListView(
+                                      padding: const EdgeInsets.only(
+                                        top: 20,
+                                        bottom: 20,
+                                      ),
+                                      shrinkWrap: true,
+                                      children: [
+                                        const Center(
+                                          child: Text(
+                                            'Berhasil menambahkan event baru!',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          height: 20,
+                                        ),
+                                        TextButton(
+                                          child: const Text(
+                                            'Close',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                });
+                          },
+                          onError: (error) {
+                            print(error);
+                            return showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return Dialog(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    elevation: 15,
+                                    child: ListView(
+                                      padding: const EdgeInsets.only(
+                                        top: 20,
+                                        bottom: 20,
+                                      ),
+                                      shrinkWrap: true,
+                                      children: [
+                                        const Center(
+                                          child: Text(
+                                            "Tidak dapat menambahkan event karena suatu hal!",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          height: 20,
+                                        ),
+                                        TextButton(
+                                          child: const Text(
+                                            "Close",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                        )
+                                      ],
+                                    ),
+                                  );
+                                });
+                          },
+                        );
+                      }
+                    })
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//show datetime picker
+// Future pickDateTime() async {
+//   DateTime? date = await pickDate();
+//   if (date == null) return;
+//   TimeOfDay? time = await pickTime();
+//   if (time == null) return;
+//   final dateTime = 
+
+// }
+
+// Future<DateTime?> pickDate() => showDatePicker(
+//       context: context,
+//       initialDate: DateTime.now(),
+//       firstDate: DateTime(2022),
+//       lastDate: DateTime(2100),
+//     );
+
+// Future<TimeOfDay?> pickTime() => showTimePicker(
+//       context: context,
+//       initialTime: TimeOfDay.now(),
+//     );
